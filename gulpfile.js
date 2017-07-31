71f199ad6344413298aad58d439b2f5d4398bc22
Derek Bell <luicd@lucid.com.au>
2017-07-31 14:15:17 +1000

    Base boilerplace and sass

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+// Gulp & utilities
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
+//var browserSync = require('browser-sync').create();
+
+// CSS
+var sass = require('gulp-sass');
+// var autoprefixer = require('gulp-autoprefixer');
+//var csso = require('gulp-csso');
+
+
+// Browser Sync
+var browserSync = require('browser-sync').create();
+
+/* ================= */
+/* CSS Files */
+
+// Compile SASS and Autoprefix.
+gulp.task('sass', function() {
+  gulp.src('assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    // Minify CSS
+    //.pipe(csso())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
+});
+
+
+
+// Browser Sync
+// see https://www.browsersync.io/docs/gulp/
+/*
+// Dynamic server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "yourlocal.dev"
+    });
+});
+*/
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+/* ================= */
+/* Gulp Watch */
+
+gulp.task('watch', function() {
+  // watch scss files
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+
+});
+
+
+/*gulp.task('default', function() {
+  // place code for your default task here
+});
+*/
+gulp.task('default', ['sass', 'browser-sync','watch']);
